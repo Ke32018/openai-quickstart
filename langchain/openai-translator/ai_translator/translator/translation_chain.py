b56dca6ab3fb0a0efba2061109c56453c06323da
leike <Ke_lei@foxmail.com>
2023-12-29 17:42:50 +0800

    translator 修改内容

--- langchain/openai-translator/ai_translator/translator/translation_chain.py
+++ langchain/openai-translator/ai_translator/translator/translation_chain.py
@@ -13,9 +13,10 @@ class TranslationChain:
     def __init__(self, model_name: str = "gpt-3.5-turbo", verbose: bool = True):
         
         # 翻译任务指令始终由 System 角色承担
+        
         template = (
             """You are a translation expert, proficient in various languages. \n
-            Translates {source_language} to {target_language}."""
+            Translates {source_language} to {target_language} using {style} style"""
         )
         system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
@@ -33,13 +34,14 @@ class TranslationChain:
 
         self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
-    def run(self, text: str, source_language: str, target_language: str) -> (str, bool):
+    def run(self, text: str, source_language: str, target_language: str, style: str) -> (str, bool):
         result = ""
         try:
             result = self.chain.run({
                 "text": text,
                 "source_language": source_language,
                 "target_language": target_language,
+                "style": style,
             })
         except Exception as e:
             LOG.error(f"An error occurred during translation: {e}")
